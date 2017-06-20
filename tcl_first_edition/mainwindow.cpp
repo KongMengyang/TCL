a77c5e9dee1a6c5ffc7eaeca1b2422d33b230e1e
孔梦阳 <331646500@qq.com>
2017-06-20 16:14:07 +0800

    调试

--- tcl_first_edition/mainwindow.cpp
+++ tcl_first_edition/mainwindow.cpp
@@ -761,27 +761,7 @@ QImage MatToQImage(const cv::Mat& mat)
         return QImage();
     }
 }
-void MainWindow::QImageToMat()
-{
-    cv::Mat mat;
-    QImage image=originimage;
-    switch (image.format())
-    {
-    case QImage::Format_ARGB32:
-    case QImage::Format_RGB32:
-    case QImage::Format_ARGB32_Premultiplied:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC4, (void*)image.constBits(), image.bytesPerLine());
-        break;
-    case QImage::Format_RGB888:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC3, (void*)image.constBits(), image.bytesPerLine());
-        cv::cvtColor(mat, mat, CV_BGR2RGB);
-        break;
-    case QImage::Format_Indexed8:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC1, (void*)image.constBits(), image.bytesPerLine());
-        break;
-    }
-    originmat=mat;
-}
+
 
 
 
